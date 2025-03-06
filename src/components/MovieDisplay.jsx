45a4d9a1b2693198e8bed36a7bc6323d8907137f
midbahou <midbahou@gmail.com>
2025-03-05 23:21:20 -0500

    updated

--- src/components/MovieDisplay.jsx
+++ src/components/MovieDisplay.jsx
@@ -7,9 +7,9 @@ function MovieDisplay({ movie }) {
         <>
         <h1>{movie.Title}</h1>
         <h2>{movie.Genre}</h2>
-        <img src={movie.Poster} alt={movie.Title} />
         <h2>{movie.Year}</h2>
         <h2>Rating: {movie.Ratings[0].Value}</h2>
+        <img src={movie.Poster} alt={movie.Title} />
         </>
     )
 };
